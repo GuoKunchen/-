52969c443ece06325e24ced42ae1767124ff2721
GuoKunchen <3200105319@zju.edu.cn>
2023-04-21 12:56:17 +0800

    App.js增加了身份验证功能,未登录无法查看个人信息和购物清单 agent.js中修改了token的存储方式将Token保存在localStorage中 对于Token过期的问题还没有解决

--- frontend/src/agent.js
+++ frontend/src/agent.js
@@ -8,34 +8,85 @@ const API_ROOT = 'http://10.214.241.122:8080';
 const encode = encodeURIComponent;
 const responseBody = res => res.body;
 
-let token ="Bearer eyJhbGciOiJIUzI1NiJ9.eyJqdGkiOiJkNTBlYmMyOC0zNTJlLTQ2NjgtOTQ3MS1hZTlhYmNjYzhjOTYiLCJzdWIiOiI4IiwiaXNzIjoiemp1bHNzMiIsImlhdCI6MTY4MTQ1ODkxNSwiZXhwIjoxNjgxNDYyNTE1fQ.9yIXyXbXyF9HV0ljkUXInxUaht0fYW0M6nr3E8ccPNE"
+// let token ="Bearer eyJhbGciOiJIUzI1NiJ9.eyJqdGkiOiJkNTBlYmMyOC0zNTJlLTQ2NjgtOTQ3MS1hZTlhYmNjYzhjOTYiLCJzdWIiOiI4IiwiaXNzIjoiemp1bHNzMiIsImlhdCI6MTY4MTQ1ODkxNSwiZXhwIjoxNjgxNDYyNTE1fQ.9yIXyXbXyF9HV0ljkUXInxUaht0fYW0M6nr3E8ccPNE"
+let token = ""
 
 const tokenPlugin = req => {
+	token = localStorage.getItem('token')
     if (token) {
         req.set('Authorization', `${token}`);
     }
 }
 
+const queue = []; // 请求队列
+let isRefreshing = false; // 是否正在更新 token
+let refreshPromise = null; // 更新 token 的 Promise
+
+const handleError = err => {
+	console.error(err);
+  };
+
 const requests = {
-    del: url =>
-        superagent.del(`${API_ROOT}${url}`).use(tokenPlugin).then(responseBody),
-    get: url =>
-        superagent.get(`${API_ROOT}${url}`).use(tokenPlugin).then(responseBody),
-    put: (url, body) =>
-        superagent.put(`${API_ROOT}${url}`, body).use(tokenPlugin).then(responseBody),
-    post: (url, body) =>
-        superagent.post(`${API_ROOT}${url}`, body).use(tokenPlugin).then(responseBody)
-};
+	del: url =>
+	  superagent
+		.del(`${API_ROOT}${url}`)
+		.use(tokenPlugin)
+		.then(responseBody)
+		.catch(handleError),
+	get: url =>
+	  superagent
+		.get(`${API_ROOT}${url}`)
+		.use(tokenPlugin)
+		.then(responseBody)
+		.catch(handleError),
+	put: (url, body) =>
+	  superagent
+		.put(`${API_ROOT}${url}`, body)
+		.use(tokenPlugin)
+		.then(responseBody)
+		.catch(handleError),
+	post: (url, body) =>
+	  superagent
+		.post(`${API_ROOT}${url}`, body)
+		.use(tokenPlugin)
+		.then(responseBody)
+		.catch(handleError),
+  };
 
 const Auth = {
     login: ( password,phoneNumber) =>
-        requests.post('/user/login', { password:password,phoneNumber:phoneNumber}),
+        requests
+			.post('/user/login', { password:password,phoneNumber:phoneNumber}),
     register: (code, password, userName, phoneNumber) =>
-        requests.post('/user/register', { code:code,password:password,userName:userName,phoneNumber:phoneNumber }),
+        requests
+			.post('/user/register', { code:code,password:password,userName:userName,phoneNumber:phoneNumber }),
     sendMessage: (phoneNumber) =>
         requests.get('/code/send,${phoneNumber}'),
     changePassword: (code, newPassword)=>
-        requests.post('/user/changePassword',{code:code,newPassword:newPassword})
+        requests.post('/user/changePassword',{code:code,newPassword:newPassword}),
+	// refreshToken: () => {
+    //     if (isRefreshing) {
+    //         // 如果正在更新 token，将当前请求放入队列中
+    //         return new Promise((resolve, reject) => {
+    //             queue.push({resolve, reject});
+    //         });
+    //     } else {
+    //         isRefreshing = true;
+    //         refreshPromise = Auth.login();
+    //         return refreshPromise.then(newToken => {
+	// 			console.log(newToken)
+    //             token = newToken.msg;
+    //             isRefreshing = false;
+    //             // 更新 token 后，处理队列中的所有请求
+    //             queue.forEach(({resolve}) => resolve(newToken));
+    //             queue.length = 0;
+    //         }).catch(err => {
+    //             isRefreshing = false;
+    //             queue.forEach(({reject}) => reject(err));
+    //             queue.length = 0;
+    //         });
+    //     }
+	// }
 };
 
 const Profile ={
@@ -56,6 +107,6 @@ const Profile ={
 export default {
     Auth,
     Profile,
-    setToken: _token => { token = _token; },
+    // setToken: _token => { token = _token; },
     token
 };
